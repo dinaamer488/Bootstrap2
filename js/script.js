d729eef379b0217c19f9f9ad152c0346292c290f
dinaamer488 <dinaamer488@gmail.com>
2024-12-14 15:10:47 +0200

    Bootstrap2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let btn=document.getElementById("btn10").onclick=function(){
+    alert("Text is copied");
+};
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  })
+ 
+
+  
+  
+    
+    
+   
